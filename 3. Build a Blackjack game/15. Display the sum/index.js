f7cee78a6a3c89d2534224e4135fd01b3a00dc70
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2022-11-19 18:13:08 -0500

    Build a Blackjack game Done (11-20)

--- 3. Build a Blackjack game/15. Display the sum/index.js
+++ 3. Build a Blackjack game/15. Display the sum/index.js
@@ -6,9 +6,12 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 // 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.querySelector('#sum-el');
+
 
 function startGame() {
     // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumEl.textContent += ' ' + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -20,3 +23,18 @@ function startGame() {
     }
     messageEl.textContent = message
 }
+
+
+/* getElementById vs querySelector */
+// On getElementById, you just pass the id name inside quotes
+// document.getElementById('sum-el');
+
+// In querySelector, you can use it for ids, classes, and tags.
+// On querySelector, you need to add a '#', before the id name inside quotes
+// document.querySelector('#sum-el');
+
+// If we want to take a class, we should use '.', instead of '#'
+// document.querySelector('.sum-el-class');
+
+// Or even you can use a whole tag
+// document.querySelector('body');
