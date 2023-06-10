a8697db6502503767816e905874526d7f1784dbe
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-06-10 11:41:37 -0500

    Build a Blackjack game Done (21-30)

--- 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
+++ 3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
@@ -2,4 +2,17 @@ let sentence = ["Hello ", "my ", "name ", "is ", "Per"]
 let greetingEl = document.getElementById("greeting-el")
 
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
+for (let i = 0; i < sentence.length; i++) {
+    greetingEl.textContent += sentence[i];
+}
 
+// ANOTHER EXERCISE: How do you keep the spaces between the words if I remve them from the array?
+/*
+let sentence = ["Hello", "my", "name", "is", "Per"] 
+let greetingEl = document.getElementById("greeting-el")
+
+// Answer:
+for (let i = 0; i < sentence.length; i++) {
+    greetingEl.textContent += sentence[i] + " "
+}
+*/
