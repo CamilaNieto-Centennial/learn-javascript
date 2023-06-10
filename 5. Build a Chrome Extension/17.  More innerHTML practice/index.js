7e6a193ef8acc6bb634dc5de5496da67bbda934e
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-06-10 18:07:05 -0500

    Build a Chrome Extension Done (11-20)

--- 5. Build a Chrome Extension/17.  More innerHTML practice/index.js
+++ 5. Build a Chrome Extension/17.  More innerHTML practice/index.js
@@ -1,7 +1,12 @@
 
 const container = document.getElementById("container")
 
-container.innerHTML = "<button>Buy!</button>"
+container.innerHTML = "<button id='buyButton'>Buy!</button>"
 
 // When clicked, render a paragraph under the button (in the container)
 // that says "Thank you for buying!"
+const buyButtonEl = document.getElementById("buyButton");
+
+buyButtonEl.addEventListener("click", ()=> {
+    container.innerHTML += "<p>Thank you for buying!</p>";
+})
