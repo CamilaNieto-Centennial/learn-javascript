99f1f69f7147cdf6974a44785bb93af4e8eb67ba
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-06-10 18:21:31 -0500

    Build a Chrome Extension Done (21-30)

--- 5. Build a Chrome Extension/23. Add the a tag/index.js
+++ 5. Build a Chrome Extension/23. Add the a tag/index.js
@@ -5,15 +5,16 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    // Clear out the input field
-    inputEl.value = "";
+    inputEl.value = ""
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+        listItems += `<li><a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a></li>`
     }
     ulEl.innerHTML = listItems  
 }
